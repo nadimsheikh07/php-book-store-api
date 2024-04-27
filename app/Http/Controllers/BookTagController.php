ed92229ed35eee8d48295547ee37e59cc68e106b
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-04-27 16:35:25 +0530

    add book master

---
+++ app/Http/Controllers/BookTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookTagRequest;
+use App\Http\Requests\UpdateBookTagRequest;
+use App\Models\BookTag;
+
+class BookTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookTagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookTag $bookTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BookTag $bookTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookTagRequest $request, BookTag $bookTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookTag $bookTag)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
 
 
 Route::group([
@@ -10,6 +15,7 @@ Route::group([
     Route::post('/signup', [UserController::class, 'signup']);
     Route::post('/signin', [UserController::class, 'signin']);
 });
+
 
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('users', UserController::class);
